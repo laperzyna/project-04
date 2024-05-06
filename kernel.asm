2f3942a0129f2278e6e588ebbd75478eca71e8c0
laperzyna <90733086+laperzyna@users.noreply.github.com>
2024-05-06 13:04:57 -0700

    added documntation for asm

--- kernel.asm
+++ kernel.asm
@@ -1,19 +1,51 @@
-; syscall is a big chunk
-; kernels responsibility to restore everything before you go back
-; kernel needs to have a way to recieve the trap reason
-; loading in a register or memory?
-; yank all registers and put them in memory before anything else happens
-; Need a new instruction to tell CPU where that memory address is 
-
-; kernel.asm
-; needs to read in the instructions
-; needs to be able to respond to CPU emu
-; It needs to know the reason in order to know how to respond
-; it does that by doing a syscall check on the number 
-; It needs to keep track of the timer
-; can use read and write instructions
-; syscall is used in prime because it doesn't have access
-
+/*
+ * The kernel.asm file mainly focuses on handling system calls
+ * and traps. It interactes directly with the CPU emulator, kernel.go. 
+ * The core functionalities include loading in the program or instructions,
+ * trap handling, and responses to various system events.
+ *
+ * 1. Overview
+ *
+ * Our kernel implementation is designed to handle system traps by interacting with our CPU
+ * emulator and being told when to execute. However, based on our CPU emulator implementation, * our kernel is fired regularly after every illegal request, syscall, or timer fired.
+ * Our kernel is responsible for managing the CPU state, memory,
+ * and other system-level functions.
+ *
+ *
+ * 2. Syscall Handling
+ *
+ * System calls are extremely important for user programs to interact with
+ * the operating system. Our kernel utilizes a comprehensive set of handlers
+ * that interpret syscall and respond with appropriate actions.
+ * Each syscall has a unique identifier, and the kernel uses this identifier
+ * to execute the appropriate response.
+ * The following is an example of a handle and response action taken by the kernel:
+ *
+ * eq r5 0 r0
+ * loadLiteral .read_instruct r3
+ * cmove r0 r3 r7
+ *
+ * 
+ * 3. Trap Handling
+ *
+ * Our kernel also utilizes a trap handling routine that captures and
+ * categorizes traps based on their origin and type. The kernel then decides
+ * the appropriate response for each type of trap, whether it is a syscall,
+ * timer fired event, or a memory fault. The handler also ensures that all system
+ * state changes are safely stored and restored, allowing for consistent
+ * system behavior.
+ *
+ * 4. Program Loading and Execution
+ *
+ * A simple loader is implemented that utilizes previous code from our bootloader.asm file to read executable instructions into memory.
+ * Our loader establishes the length of the program, reads in the program instructions, and sets the instruction pointer to the start of the program. The labels that take care of this functionality are:
+ *       start,
+ *      instruc_loadin,
+ *      
+ *
+ *
+ * Lidia Perzyna & Isaac Meltsner (Intro to Security USFCA-2024)
+ */
 
 start: 
     ; sent trap handler address
@@ -81,6 +113,7 @@ instruc_loadin:
 
 
 trap_handler_store:
+    ; store all registers at a location in memory
     store r0 0
     store r1 1
     store r2 2
@@ -90,58 +123,67 @@ trap_handler_store:
 
     ; sending memory address to CPU '6' stands for the c.memory[num] on the CPU side
     load 6 r5
+    ; prep the address to jump to after handling the trap
     loadLiteral .trap_reset r4
-    ; Check the trap reason and handle (all the checks/ hooks)
 
+    ; Check the trap reason and handle (all the checks/ hooks)
     ; if 0, run read 
     eq r5 0 r0
     loadLiteral .read_instruct r3
+    ; jump to read handler if trap reason is 0
     cmove r0 r3 r7
 
     ; if 1, run write
     eq r5 1 r0
     loadLiteral .write_instruct r3
+    ; jump to write handler if trap reason is 1
     cmove r0 r3 r7
 
     ; if 2, then halt
     eq r5 2 r0
     loadLiteral .halt r3
+    ; jump to halt routine if trap reason is 2
     cmove r0 r3 r7
 
     ; if 3, timer
     eq r5 3 r0
     loadLiteral .timer_fired r3
+    ; jump to timer fired routine if trap reason is 3
     cmove r0 r3 r7
-    ; cmove r0 .timer_fired r7
 
     ; if 4, throw memory out of bounds
     eq r5 4 r0 
+    ; jump to memory bounds error routine if trap reason is 4
     cmove r0 .mem_bounds r7
 
     ; if 5, throw illegal instruction
     eq r5 5 r0
+    ; jump to illegal instruction routine if trap reason is 5
     cmove r0 .illegal_instruc r7
 
     eq r5 8 r0
     loadLiteral .timer_fired_num r3
+    ; special handling for a specific case trap reason 8
     cmove r0 r3 r7
 
     ; unreachable syscall
     move r4 r7
     
-
+; handle a read request from the trap
 read_instruct:
     ; allow read 
     read r6
     ; jump to exit
     move r4 r7
 
+; handle a write request from the trap
 write_instruct:
     ; allow read 
     write r6
     ; jump to exit
     move r4 r7
 
+; handle illegal instruction trap
 illegal_instruc:
     ; \nIllegal instruction!
     ; \nTimer fired XXXXXXXX times\n
@@ -171,6 +213,7 @@ illegal_instruc:
     loadLiteral .timer_fired_num r0
     move r0 r7
 
+; handle memory out of bounds access
 mem_bounds:
 ; \nOut of bounds memory access!
 ; \nTimer fired XXXXXXXX times\n
@@ -208,6 +251,7 @@ mem_bounds:
     loadLiteral .timer_fired_num r0
     move r0 r7
 
+; handle a halt trap
 halt:
     ; \nProgram has exited
     ; \nTimer fired XXXXXXXX times\n
@@ -235,6 +279,7 @@ halt:
     loadLiteral .timer_fired_num r0
     move r0 r7
 
+; handle timer fired trap
 timer_fired:
     ; \nTimer fired!\n
     write 10    ; new line
@@ -255,7 +300,9 @@ timer_fired:
     ; jump to reset
     move r4 r7
 
+; begin printing the number of times the timer fired
 timer_fired_num:
+   ; \nTimer fired XXXXXX times\n
     write 10    ; new line
 	write 'T'
 	write 'i'
@@ -270,9 +317,14 @@ timer_fired_num:
 	write 'd'
 	write 32
 
+    ; load the number of timer fires from a memory location (index 8) into r0
 	load 8 r0
+    ; load the initial bit shift value (28) into r1
 	loadLiteral 28 r1
 
+
+; enter the timer_loop to convert a timer value to hexadecimal. 
+; utilizes bitwise operations (shr, and) to extract and manipulate 4-bit hexadecimal digits
 timer_loop:
 	shr r0 r1 r2
 	and r2 15 r2
@@ -282,35 +334,56 @@ timer_loop:
 	cmove r3 r5 r7
 	add r2 87 r2
 
-continue:
-	write r2
-	sub r1 4 r1	
+; manage loop 
+; if there are more digits to check then loop through again if not move to continue
+loop_again:
+    ; write the current hex digit stored in r2
+    write r2
+    ; subtract 4 from the shift amount in r1 for the next hex digit
+    sub r1, 4, r1
 
-	eq r1 0 r3
-	loadLiteral .timer_two r5
-	cmove r3 r5 r7
-	loadLiteral .timer_loop r5
-	move r5 r7
+    ; check if all hex digits have been processed (when shift amount reaches 0)
+    eq r1, 0, r3
+    ; load address of 'timer_two' section into r5 for handling the last digit
+    loadLiteral .timer_two, r5
+    ; conditional move: if all digits processed, move to process the last digit
+    cmove r3, r5, r7
+    ; otherwise, repeat the loop
+    loadLiteral .timer_loop, r5
+    move r5, r7
 
+; adjust the ASCII value to show the actual hexadecimal digit
 hex:
-	add r2 48 r2
-	loadLiteral .continue r5
-	move r5 r7	
-	
+    ; ddd 48 to r2 to adjust ASCII value for numeric digits (0-9)
+    add r2, 48, r2
+    ; jump back to 'loop_again' to continue processing
+    loadLiteral .loop_again, r5
+    move r5, r7
+
+; conversion to hex
 timer_two:
-	and r0 15 r2
-	lt r2 10 r3
+	; isolate the last hex digit from r0
+    and r0, 15, r2
+    ; check if it is a numeric digit
+    lt r2, 10, r3
 
-	loadLiteral .next_hex r5
-	cmove r3 r5 r7
-	add r2 87 r2
-	loadLiteral .timer_finish r5
-	move r5 r7
+    ; prepare to handle numeric or alphabetic hex digit
+    loadLiteral .next_hex, r5
+    cmove r3, r5, r7
+    ; adjust for alphabetic digit (a-f)
+    add r2, 87, r2
+    ; prepare to finish displaying timer value
+    loadLiteral .timer_finish, r5
+    move r5, r7
 
+; finish conversion
 next_hex:
+    ; adjust ASCII for numeric digit
 	add r2 48 r2
 
+; finish up timer - print number "times" and halt
 timer_finish:
+    ; write the final digit
 	write r2				
 	write 32
 	write 't'
@@ -319,8 +392,10 @@ timer_finish:
 	write 'e'
 	write 's'
 	write 10
-	halt 					; Finally: halt!
+    ; halt the program
+	halt
 
+; reset registers and reset the instruction pointer and set the mode back to user
 trap_reset:
     ; Reset registers
     load 0 r0
